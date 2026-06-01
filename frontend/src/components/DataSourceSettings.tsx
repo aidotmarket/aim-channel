9c653faccaaf4d342005b6d5ed42739a66319e59
Max Robbins <max@Koskadeux.local>
2026-06-01 10:16:08 +0200

    Expand AWS S3 region dropdown to full commercial region set (S745)

--- frontend/src/components/DataSourceSettings.tsx
+++ frontend/src/components/DataSourceSettings.tsx
@@ -66,14 +66,46 @@ interface VerifyResult {
 }
 
 const REGIONS = [
+  // US
   "us-east-1",
   "us-east-2",
   "us-west-1",
   "us-west-2",
+  // Canada
+  "ca-central-1",
+  "ca-west-1",
+  // South America
+  "sa-east-1",
+  // Europe
   "eu-west-1",
+  "eu-west-2",
+  "eu-west-3",
   "eu-central-1",
-  "ap-southeast-1",
+  "eu-central-2",
+  "eu-north-1",
+  "eu-south-1",
+  "eu-south-2",
+  // Middle East / Israel
+  "me-south-1",
+  "me-central-1",
+  "il-central-1",
+  // Africa
+  "af-south-1",
+  // Asia Pacific
+  "ap-east-1",
+  "ap-south-1",
+  "ap-south-2",
   "ap-northeast-1",
+  "ap-northeast-2",
+  "ap-northeast-3",
+  "ap-southeast-1",
+  "ap-southeast-2",
+  "ap-southeast-3",
+  "ap-southeast-4",
+  "ap-southeast-5",
+  "ap-southeast-7",
+  // Mexico
+  "mx-central-1",
 ];
 
 function apiHeaders(): Record<string, string> {
